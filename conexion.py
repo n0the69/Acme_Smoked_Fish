3b3c1b32f193de9ac6606a57f43167652a5fb6d2
luis-carrasco <luis.carrasco107@inacapmail.cl>
2026-04-28 08:12:17 -0400

    actualizando push

--- conexion.py
+++ conexion.py
@@ -1,12 +1,24 @@
-from pymongo import MongoClient
+#from pymongo import MongoClient
 
-#conexion local
-cliente = MongoClient('mongodb://localhost:27017/')
+# conexion local
+#cliente = MongoClient('mongodb://localhost:27017/')
 
-#crear o usar base de datos
-db = cliente['py_mongodb']  
+# crear o usar base de datos
+#db = cliente['py_mongodb']  
 
-#crear coleccion
-coleccion = db['Acme_Smoked_fish']
+# crear coleccion
+#coleccion = db['Acme_Smoked_fish']
 
-print ("Base de datos y colección creadas exitosamente.")
+#print ("Base de datos y colección creadas exitosamente.")
+
+import pymongo
+
+class ConexionDB:
+
+    def __init__(self):
+        self.client = pymongo.MongoClient("mongodb://localhost:27017/")
+        self.db = self.client["py_mongodb"]
+        self.coleccion = self.db["Acme_Smoked_fish"]
+        
+    def obtener_db(self):
+        return self.db
